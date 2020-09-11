7551783fe691bbde3ec888affcf10d98e0a7543f
Gecko736 <38330197+Gecko736@users.noreply.github.com>
2020-09-11 15:50:41 -0500

    Update UIAlert.swift

--- UIAlert.swift
+++ UIAlert.swift
@@ -10,28 +10,42 @@ extension UIAlertAction {
     public static func OK(_ handler: @escaping (UIAlertAction) -> ()) -> UIAlertAction {
         return UIAlertAction(title: "OK", handler: handler)
     }
+	
+	public static let CANCEL = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+	
+	public static func CANCEL(_ handler: @escaping (UIAlertAction) -> ()) -> UIAlertAction {
+		return UIAlertAction(title: "Cancel", style: .cancel, handler: handler)
+	}
 }
 
-class UIAlert {
-    static func show(error: Error, from sender: UIViewController) {
-        show(title: "Error", message: error.localizedDescription, sender: sender)
-    }
-    
-    static func show(title: String, message: String, sender: UIViewController) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(.OK)
-        sender.present(alert, animated: true, completion: nil)
-    }
-    
-    static func show(title: String, message: String, sender: UIViewController, okAction: @escaping () -> ()) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(.OK { _ in okAction() })
-        sender.present(alert, animated: true, completion: nil)
-    }
-    
-    static func show(title: String, message: String, sender: UIViewController, actions: [UIAlertAction]) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        for action in actions { alert.addAction(action) }
-        sender.present(alert, animated: true, completion: nil)
-    }
+extension UIViewController {
+	func popUp(error: Error, _ okAction: @escaping () -> () = { }) {
+		popUp(title: "Error", message: error.localizedDescription, okAction: okAction)
+	}
+	
+	func popUp(title: String, message: String? = nil) {
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		alert.addAction(.OK)
+		present(alert, animated: true, completion: nil)
+	}
+	
+	func popUp(title: String, message: String? = nil, okAction: @escaping () -> ()) {
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		alert.addAction(.OK { _ in okAction() })
+		present(alert, animated: true, completion: nil)
+	}
+	
+	func popUp(title: String, message: String? = nil, actions: [UIAlertAction]) {
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		for action in actions { alert.addAction(action) }
+		present(alert, animated: true, completion: nil)
+	}
+	
+	func actionSheet(_ actions: [UIAlertAction]) {
+		let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+		for action in actions {
+			alert.addAction(action)
+		}
+		present(alert, animated: true, completion: nil)
+	}
 }
